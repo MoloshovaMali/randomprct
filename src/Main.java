31ac9c5c4d4dcc18a40ab71475de29f93d8e1e71
github <mmoloshova@gmail.com>
2023-03-15 20:54:38 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        int[]arr=new int[]{2,1,4,3,6,5};
+        int change = arr[0];
+        arr[0] = arr[1];
+        arr[1] = change;
+        int b=arr[2];
+        arr[2]=arr[3];
+        arr[3]=b;
+        int c=arr[4];
+        arr[4]=arr[5];
+        arr[5]=c;
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+}
